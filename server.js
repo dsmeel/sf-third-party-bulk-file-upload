69be3c755822496f1102417505d0f4d6305a23b9
Dharmendra <singh.dharmendra390@gmail.com>
2023-08-15 22:33:22 +0530

    request body debug

--- server.js
+++ server.js
@@ -176,8 +176,27 @@ app.post('/third-party-request', async (req, res) => {
             res.json({ status: 'success', data: response.data });
         })
         .catch(function (error) {
-            console.log(error);
-            res.json({ status: 'error', data: error.response });
+            var errorToReturn = { status: 'error' };
+            if (error.response) {
+                // The request was made and the server responded with a status code
+                // that falls out of the range of 2xx
+                errorToReturn.status = error.response.status;
+                errorToReturn.data = error.response.data;
+                console.log(error.response.data);
+                console.log(error.response.status);
+                console.log(error.response.headers);
+              } else if (error.request) {
+                // The request was made but no response was received
+                // `error.request` is an instance of XMLHttpRequest in the browser 
+                // and an instance of http.ClientRequest in node.js
+                errorToReturn.data = error.response;
+                console.log(error.request);
+              } else {
+                // Something happened in setting up the request that triggered an Error
+                console.log('Error', error.message);
+              }
+              errorToReturn.message = error.message;
+            res.json(errorToReturn);
         });
 
 
