f366433fcea1414eeba7218b2148a5ab3b53d728
Dharmendra <singh.dharmendra390@gmail.com>
2023-08-15 19:56:59 +0530

    fix format of body

--- server.js
+++ server.js
@@ -113,7 +113,7 @@ app.post('/third-party-request', async (req, res) => {
 
 
 
-    if (req.body.format.toLowerCase() == "form-data" || (req.body.files != undefined && req.body.files.length > 0)) {
+    if ((req.body.format && req.body.format.toLowerCase() == "form-data") || (req.body.files != undefined && req.body.files.length > 0)) {
         var data = new FormData();
         for (let key in req.body.requestBody) {
             data.append(key, req.body.requestBody[key]);
